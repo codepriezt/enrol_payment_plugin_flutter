17d67a44034d4e121b1301d45531fe02af54aaeb
codepriezt <kolumide22@gmail.com>
2019-11-18 14:59:43 +0100

    url update

--- enrolment_form.php
+++ enrolment_form.php
@@ -117,7 +117,7 @@ function payWithRave(e){
                         'data': data,
                         'ud':udf1
                     };
-                    window.location.href =`http://moodle.digondigital.com/moodle.digondigital.com/enrol/payumoney/return.php?id=${courseid}`;
+                    window.location.href =`http://prodx.org/enrol/payumoney/return.php?id=${courseid}`;
                 } else {
                     console.log(data);
                 }
