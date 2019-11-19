68c9dfce14170d10fcc3b4f01548c608d71401a0
codepriezt <kolumide22@gmail.com>
2019-11-19 12:31:52 +0100

    commit

--- enrolment_form.php
+++ enrolment_form.php
@@ -136,16 +136,17 @@ function payWithRave(e){
             var httpc = new XMLHttpRequest();
             var url = $('#url').val();
             console.log(url);
-            console.log(verifydata);
+            var data = JSON.stringify(verifydata);
+            console.log(data);
             httpc.open("POST" , url , true);
-            httpc.setRequestHeader("Content-Length", params.length);
+            httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             httpc.onreadystatechange = function() { 
            if(httpc.readyState == 4 && httpc.status == 200) { 
             console.log(httpc.responseText); 
         }
     };
     
-     httpc.send(verifydata);
+     httpc.send(data);
     }
 
 </script>
