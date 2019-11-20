041ba2cc08db62e88e26e367b09116460dd84a39
codepriezt <kolumide22@gmail.com>
2019-11-21 00:31:29 +0100

    slsoc

--- record.php
+++ record.php
@@ -8,15 +8,15 @@ require_once($CFG->libdir . '/filelib.php');
 global $DB, $CFG ;
 
 
-$enrolpayumoney = new stdClass();
+
 $decoded = $_POST['user'];
 var_dump($decoded);
-// $enrolpayumoney->auth_json= json_encode($decoded);
-// $enrolpayumoney->timeupdated = time();
-// $ret1 = $DB->insert_record("enrol_payumoney", $enrolpayumoney, true);
-var_dump($ret1);
+// // $enrolpayumoney->auth_json= json_encode($decoded);
+// // $enrolpayumoney->timeupdated = time();
+// // $ret1 = $DB->insert_record("enrol_payumoney", $enrolpayumoney, true);
+// var_dump($ret1);
 
-echo $ret1;
+// echo $ret1;
 
 
 ?>
