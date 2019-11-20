b7abffd471d888b42030f73a2751bbd2fbbefde8
codepriezt <kolumide22@gmail.com>
2019-11-21 00:48:00 +0100

    ff

--- record.php
+++ record.php
@@ -1,5 +1,11 @@
 <?php
+require("../../config.php");
+require_once("lib.php");
+require_once($CFG->libdir.'/eventslib.php');
+require_once($CFG->libdir.'/enrollib.php');
+require_once($CFG->libdir . '/filelib.php');
 
+global $DB, $CFG ;
 
 
 
