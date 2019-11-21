9641e6ac440e0640439697af72e23b3f93420ba1
codepriezt <kolumide22@gmail.com>
2019-11-21 12:08:43 +0100

    ggor

--- record.php
+++ record.php
@@ -1,10 +1,29 @@
 <?php
 
+define('NO_DEBUG_DISPLAY', true);
+
+require("../../config.php");
+require_once("lib.php");
+require_once($CFG->libdir.'/eventslib.php');
+require_once($CFG->libdir.'/enrollib.php');
+require_once($CFG->libdir . '/filelib.php');
+
+global $DB, $CFG ;
+
+
 $decoded = file_get_contents("php://input");
 
-$object  = json_decode($decoded);
+$enrolpayumoney = new stdClass();
 
-var_dump($object);
+$enrolpayumoney->auth_json = json_encode($decoded);
+$enrolpayumoney->timeupdated = time();
+
+
+$ret1 = $DB->insert_record("enrol_payumoney_nigeria", $enrolpayumoney, true);
+
+var_dump($ret1);
+
+
 
 
 
