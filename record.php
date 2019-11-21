55859eb44bcebf7622cad8de079ed2e70d7954c3
codepriezt <kolumide22@gmail.com>
2019-11-21 16:02:43 +0100

    dolo

--- record.php
+++ record.php
@@ -3,7 +3,6 @@ define('NO_DEBUG_DISPLAY', false);
 
 require("../../config.php");
 require_once("lib.php");
-require_once($CFG->libdir.'/eventslib.php');
 require_once($CFG->libdir.'/enrollib.php');
 require_once($CFG->libdir . '/filelib.php');
 
@@ -20,7 +19,9 @@ $enrolpayumoney->timeupdated = time();
 
 $ret1 = $DB->insert_record("enrol_payumoney", $enrolpayumoney, true);
 
-var_dump($ret1);
+print_r($ret1);
+
+var_dump($ret1)
 
 
 
