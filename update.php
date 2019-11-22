3064c58f3df92767c6bd19453ae9b6a7762d4383
codepriezt <kolumide22@gmail.com>
2019-11-22 01:19:27 +0100

    nnos

--- update.php
+++ update.php
@@ -55,154 +55,149 @@ $contextid= $responsearray['contextid'];
 $instanceid = $responsearray['instanceid'];
 
 
-if (! $user = $DB->get_record("user", array("id" => $userid))) {
-    print_error("Not a valid user id"); die;
-}
+// if (! $user = $DB->get_record("user", array("id" => $userid))) {
+//     print_error("Not a valid user id"); die;
+// }
 
-if (! $course = $DB->get_record("course", array("id"=> $contextid))) {
-    print_error("Not a valid course id"); die;
-}
+// if (! $course = $DB->get_record("course", array("id"=> $contextid))) {
+//     print_error("Not a valid course id"); die;
+// }
 
-if (! $context = context_course::instance($contextid, IGNORE_MISSING)) {
-    print_error("Not a valid context id"); die;
-}
+// if (! $context = context_course::instance($contextid, IGNORE_MISSING)) {
+//     print_error("Not a valid context id"); die;
+// }
 
-if (! $plugininstance = $DB->get_record("enrol", array("id" => $instanceid, "status" => 0))) {
-    print_error("Not a valid instance id"); die;
-}
-
-
-$enrolflutter = $userenrolments = $roleassignments = new stdClass();
-
-$enrolflutter->id = $id;
-$enrolflutter->courseid = $courseid;
-$enrolflutter->userid = $userid;
-$enrolflutter->instanceid = $instanceid;
-$enrolflutter->amount = $amount;
-//$enrolpayumoney->tax = $responsearray['tax'];
+// if (! $plugininstance = $DB->get_record("enrol", array("id" => $instanceid, "status" => 0))) {
+//     print_error("Not a valid instance id"); die;
+// }
 
 
+// $enrolflutter = $userenrolments = $roleassignments = new stdClass();
 
-if ($status == "successful") {
-    $enrolflutter->status = 'Approved';
+// $enrolflutter->id = $id;
+// $enrolflutter->courseid = $courseid;
+// $enrolflutter->userid = $userid;
+// $enrolflutter->instanceid = $instanceid;
+// $enrolflutter->amount = $amount;
+// //$enrolpayumoney->tax = $responsearray['tax'];
+
+
+
+// if ($status == "successful") {
+//     $enrolflutter->status = 'Approved';
     
-    $PAGE->set_context($context);
-    $coursecontext = context_course::instance($course->id, IGNORE_MISSING);
+//     $PAGE->set_context($context);
+//     $coursecontext = context_course::instance($course->id, IGNORE_MISSING);
 
-    if ($users = get_users_by_capability($context, 'moodle/course:update', 'u.*', 'u.id ASC',
-                                         '', '', '', '', false, true)) {
-        $users = sort_by_roleassignment_authority($users, $context);
-        $teacher = array_shift($users);
-    } else {
-        $teacher = false;
-    }
+//     if ($users = get_users_by_capability($context, 'moodle/course:update', 'u.*', 'u.id ASC',
+//                                          '', '', '', '', false, true)) {
+//         $users = sort_by_roleassignment_authority($users, $context);
+//         $teacher = array_shift($users);
+//     } else {
+//         $teacher = false;
+//     }
 
-    $plugin = enrol_get_plugin('flutter');
+//     $plugin = enrol_get_plugin('flutter');
 
    
   
 
-    if (!empty($mailstudents)) {
-        $a = new stdClass();
-        $a->coursename = format_string($course->fullname, true, array('context' => $coursecontext));
-        $a->profileurl = "$CFG->wwwroot/user/view.php?id=$user->id";
+//     if (!empty($mailstudents)) {
+//         $a = new stdClass();
+//         $a->coursename = format_string($course->fullname, true, array('context' => $coursecontext));
+//         $a->profileurl = "$CFG->wwwroot/user/view.php?id=$user->id";
 
-        $eventdata = new \core\message\message();
-        $eventdata->courseid          = $course->id;
-        $eventdata->modulename        = 'moodle';
-        $eventdata->component         = 'enrol_flutter';
-        $eventdata->name              = 'flutter_enrolment';
-        $eventdata->userfrom          = empty($teacher) ? core_user::get_noreply_user() : $teacher;
-        $eventdata->userto            = $user;
-        $eventdata->subject           = get_string("enrolmentnew", 'enrol', $shortname);
-        $eventdata->fullmessage       = get_string('welcometocoursetext', '', $a);
-        $eventdata->fullmessageformat = FORMAT_PLAIN;
-        $eventdata->fullmessagehtml   = '';
-        $eventdata->smallmessage      = '';
-        message_send($eventdata);
+//         $eventdata = new \core\message\message();
+//         $eventdata->courseid          = $course->id;
+//         $eventdata->modulename        = 'moodle';
+//         $eventdata->component         = 'enrol_flutter';
+//         $eventdata->name              = 'flutter_enrolment';
+//         $eventdata->userfrom          = empty($teacher) ? core_user::get_noreply_user() : $teacher;
+//         $eventdata->userto            = $user;
+//         $eventdata->subject           = get_string("enrolmentnew", 'enrol', $shortname);
+//         $eventdata->fullmessage       = get_string('welcometocoursetext', '', $a);
+//         $eventdata->fullmessageformat = FORMAT_PLAIN;
+//         $eventdata->fullmessagehtml   = '';
+//         $eventdata->smallmessage      = '';
+//         message_send($eventdata);
 
-    }
+//     }
 
-    if (!empty($mailteachers) && !empty($teacher)) {
-        $a->course = format_string($course->fullname, true, array('context' => $coursecontext));
-        $a->user = fullname($user);
+//     if (!empty($mailteachers) && !empty($teacher)) {
+//         $a->course = format_string($course->fullname, true, array('context' => $coursecontext));
+//         $a->user = fullname($user);
 
-        $eventdata = new \core\message\message();
-        $eventdata->courseid          = $course->id;
-        $eventdata->modulename        = 'moodle';
-        $eventdata->component         = 'enrol_payumoney';
-        $eventdata->name              = 'payumoney_enrolment';
-        $eventdata->userfrom          = $user;
-        $eventdata->userto            = $teacher;
-        $eventdata->subject           = get_string("enrolmentnew", 'enrol', $shortname);
-        $eventdata->fullmessage       = get_string('enrolmentnewuser', 'enrol', $a);
-        $eventdata->fullmessageformat = FORMAT_PLAIN;
-        $eventdata->fullmessagehtml   = '';
-        $eventdata->smallmessage      = '';
-        message_send($eventdata);
-    }
+//         $eventdata = new \core\message\message();
+//         $eventdata->courseid          = $course->id;
+//         $eventdata->modulename        = 'moodle';
+//         $eventdata->component         = 'enrol_payumoney';
+//         $eventdata->name              = 'payumoney_enrolment';
+//         $eventdata->userfrom          = $user;
+//         $eventdata->userto            = $teacher;
+//         $eventdata->subject           = get_string("enrolmentnew", 'enrol', $shortname);
+//         $eventdata->fullmessage       = get_string('enrolmentnewuser', 'enrol', $a);
+//         $eventdata->fullmessageformat = FORMAT_PLAIN;
+//         $eventdata->fullmessagehtml   = '';
+//         $eventdata->smallmessage      = '';
+//         message_send($eventdata);
+//     }
 
-    if (!empty($mailadmins)) {
-        $a->course = format_string($course->fullname, true, array('context' => $coursecontext));
-        $a->user = fullname($user);
-        $admins = get_admins();
-        foreach ($admins as $admin) {
-            $eventdata = new \core\message\message();
-            $eventdata->courseid          = $course->id;
-            $eventdata->modulename        = 'moodle';
-            $eventdata->component         = 'enrol_payumoney';
-            $eventdata->name              = 'payumoney_enrolment';
-            $eventdata->userfrom          = $user;
-            $eventdata->userto            = $admin;
-            $eventdata->subject           = get_string("enrolmentnew", 'enrol', $shortname);
-            $eventdata->fullmessage       = get_string('enrolmentnewuser', 'enrol', $a);
-            $eventdata->fullmessageformat = FORMAT_PLAIN;
-            $eventdata->fullmessagehtml   = '';
-            $eventdata->smallmessage      = '';
-            message_send($eventdata);
-        }
-    }
-}
-if ($responsearray['status'] == "pending") {
-    $enrolpayumoney->payment_status = 'Held for Review';
-}
-if ($responsearray['status'] == "failure") {
-    $enrolpayumoney->payment_status = 'Declined due to some Error';
-}
-
-
-$enrolflutter->txnid = $responsearray['txref'];
-
-$enrolflutter->timeupdated = time();
-/* Inserting value to enrol_payumoney table */
-$ret1 = $DB->update_record("enrol_flutter", $enrolflutter, false);
-
-if ($responsearray['status'] == "successful") {
-    /* Inserting value to user_enrolments table */
-
-    $userenrolments->status = 0;
-    $userenrolments->enrolid = $instanceid;
-    $userenrolments->userid = $userid;
-    $userenrolments->timestart = time();
-    $userenrolments->timeend = time();
-    $userenrolments->modifierid = 2;
-    $userenrolments->timecreated = time();
-    $userenrolments->timemodified = time();
-    $ret2 = $DB->insert_record("user_enrolments", $userenrolments, false);
-    /* Inserting value to role_assignments table */
-    $roleassignments->roleid = 5;
-    $roleassignments->contextid = $contextid;
-    $roleassignments->userid = $userid;
-    $roleassignments->timemodified = time();
-    $roleassignments->modifierid = 2;
-    $roleassignments->component = '';
-    $roleassignments->itemid = 0;
-    $roleassignments->sortorder = 0;
-    $ret3 = $DB->insert_record('role_assignments', $roleassignments, false);
-}
+//     if (!empty($mailadmins)) {
+//         $a->course = format_string($course->fullname, true, array('context' => $coursecontext));
+//         $a->user = fullname($user);
+//         $admins = get_admins();
+//         foreach ($admins as $admin) {
+//             $eventdata = new \core\message\message();
+//             $eventdata->courseid          = $course->id;
+//             $eventdata->modulename        = 'moodle';
+//             $eventdata->component         = 'enrol_payumoney';
+//             $eventdata->name              = 'payumoney_enrolment';
+//             $eventdata->userfrom          = $user;
+//             $eventdata->userto            = $admin;
+//             $eventdata->subject           = get_string("enrolmentnew", 'enrol', $shortname);
+//             $eventdata->fullmessage       = get_string('enrolmentnewuser', 'enrol', $a);
+//             $eventdata->fullmessageformat = FORMAT_PLAIN;
+//             $eventdata->fullmessagehtml   = '';
+//             $eventdata->smallmessage      = '';
+//             message_send($eventdata);
+//         }
+//     }
+// }
+// if ($responsearray['status'] == "pending") {
+//     $enrolpayumoney->payment_status = 'Held for Review';
+// }
+// if ($responsearray['status'] == "failure") {
+//     $enrolpayumoney->payment_status = 'Declined due to some Error';
+// }
 
 
-echo '<script type="text/javascript">
-     window.location.href="'.$CFG->wwwroot.'/enrol/payumoney/return.php?id='.$courseid.'";
-     </script>';
-die;
+// $enrolflutter->txnid = $responsearray['txref'];
+
+// $enrolflutter->timeupdated = time();
+// /* Inserting value to enrol_payumoney table */
+// $ret1 = $DB->update_record("enrol_flutter", $enrolflutter, false);
+
+// if ($responsearray['status'] == "successful") {
+//     /* Inserting value to user_enrolments table */
+
+//     $userenrolments->status = 0;
+//     $userenrolments->enrolid = $instanceid;
+//     $userenrolments->userid = $userid;
+//     $userenrolments->timestart = time();
+//     $userenrolments->timeend = time();
+//     $userenrolments->modifierid = 2;
+//     $userenrolments->timecreated = time();
+//     $userenrolments->timemodified = time();
+//     $ret2 = $DB->insert_record("user_enrolments", $userenrolments, false);
+//     /* Inserting value to role_assignments table */
+//     $roleassignments->roleid = 5;
+//     $roleassignments->contextid = $contextid;
+//     $roleassignments->userid = $userid;
+//     $roleassignments->timemodified = time();
+//     $roleassignments->modifierid = 2;
+//     $roleassignments->component = '';
+//     $roleassignments->itemid = 0;
+//     $roleassignments->sortorder = 0;
+//     $ret3 = $DB->insert_record('role_assignments', $roleassignments, false);
+// }
+
