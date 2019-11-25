f0de8bbeed5de09e21cf7d804af0e431e7d27815
codepriezt <kolumide22@gmail.com>
2019-11-25 10:37:46 +0100

    git

--- record.php
+++ record.php
@@ -9,30 +9,28 @@ require_once($CFG->libdir . '/filelib.php');
 global $DB, $CFG ;
 
 
-// $decoded = file_get_contents("php://input");
+$decoded = file_get_contents("php://input");
 
-$decoded = $_POST;
 
 var_dump($decoded);
 
-$dec = $_POST['form'];
-
-var_dump($dec);
-
-$enrolflutter = new stdClass();
-
-$enrolflutter->auth_json = json_encode($decoded);
-$enrolflutter->timeupdated = time();
 
 
 
-var_dump($enrolflutter);
+
+// $enrolflutter = new stdClass();
+
+// $enrolflutter->auth_json = json_encode($decoded);
+// $enrolflutter->timeupdated = time();
 
 
 
-$ret1 = $DB->insert_record("enrol_flutter", $enrolflutter, true);
 
-var_dump($ret1);
+
+
+// $ret1 = $DB->insert_record("enrol_flutter", $enrolflutter, true);
+
+// var_dump($ret1);
 
 // echo '<script type="text/javascript">
 //      window.location.href="'.$CFG->wwwroot.'/enrol/flutter/update.php?id='.$ret1.'";
